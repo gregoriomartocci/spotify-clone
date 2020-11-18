9a1f9c7069f84440224ce56389f341efacc821e8
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-11-18 16:01:48 -0300

    material ui

--- spotify-clone-app/src/Components/Sidebar.js
+++ spotify-clone-app/src/Components/Sidebar.js
@@ -1,9 +1,10 @@
 import React from "react";
 import "./Sidebar.css";
 import SidebarOption from "../Components/SidebarOption";
-import HomeIcon from "@material-ui/icons/Home";
+import HomeIcon from '@material-ui/icons/Home';
 import SearchIcon from "@material-ui/icons/Search";
 import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
+
 
 function Sidebar() {
   return (

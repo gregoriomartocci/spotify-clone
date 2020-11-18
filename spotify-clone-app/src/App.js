aafb5485373a4a273b5d92069ed5f07c3b510214
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-11-18 10:35:45 -0300

    building reducer and data Layer, so far is empty but we will push data

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -4,13 +4,18 @@ import "./App.css";
 import { getTokenFromUrl } from "./spotify";
 import SpotifyWebApi from "spotify-web-api-js"
 import Player from "./Components/Player"
+import {useDataLayerValue } from "./DataLayer"
 
 const spotify = new SpotifyWebApi();
 
 // Run code based on a given Condition
 
+// if i grab something from the data layer i put const [{user}]
+// dispatch is like a speciall gun to shoot at DataLayer.
+
 function App() {
   const [token, setToken] = useState(null); //short term memory store
+  const [{} , dispatch] = useDataLayerValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();

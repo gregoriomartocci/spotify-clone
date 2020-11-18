94ee89a6d7952bb77cc3d6629db6e2e6a5110fcd
Gregorio Martocci <gregoriomartocci@gmail.com>
2020-11-18 10:12:07 -0300

    starting with the Data Layer and the reducer

--- spotify-clone-app/src/App.js
+++ spotify-clone-app/src/App.js
@@ -1,11 +1,34 @@
-import Login from "./Components/Login"
-import './App.css';
+import React, { useEffect, useState } from "react";
+import Login from "./Components/Login";
+import "./App.css";
+import { getTokenFromUrl } from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js"
+import Player from "./Components/Player"
+
+const spotify = new SpotifyWebApi();
+
+// Run code based on a given Condition
 
 function App() {
+  const [token, setToken] = useState(null); //short term memory store
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token) // giving acces to the object instance
+      spotify.getMe().then(user => {
+        console.log(user)
+      })
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <div className="App">{token ? <Player/> : <Login />}</div>  // si hay token estoy loggueado
   );
 }
 
